a73e068505bca43d5d5b0914c481854342efd40b
fagnerpsantos <fagnerpinheirosantos@gmail.com>
2019-06-12 18:43:15 -0300

    05.05_Validando_Tipos_Arquivos

--- admin_blog/forms/post_form.py
+++ admin_blog/forms/post_form.py
@@ -1,8 +1,14 @@
 from django import forms
 from ..models import Post, Categoria
+from upload_validator import FileTypeValidator
 
 class PostForm(forms.ModelForm):
     categoria = forms.ModelChoiceField(queryset=Categoria.objects.all())
+    capa = forms.ImageField(
+        validators=[FileTypeValidator(
+            allowed_types=['image/jpeg']
+        )]
+    )
     class Meta:
         model = Post
         fields = ['titulo', 'descricao', 'conteudo', 'categoria', 'capa']
